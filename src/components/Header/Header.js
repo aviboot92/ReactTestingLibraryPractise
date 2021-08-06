568fa802494d7c40e95bddaac11f6fa97ef51460
avinash.k.v <avinash.k@techsophy.com>
2021-08-06 16:57:51 +0530

    Successfully written test cases for Heading Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 export default function Header({ title }) {
   return (
     <>
-      <h1 title="Header" className="header">
+      <h1 title="Header" title="header-1" alt="Main heading" className="header">
         {title}
       </h1>
       <h3 data-testid="header-2" className="header">
--- src/components/Header/__tests__/Header.test.js
+++ src/components/Header/__tests__/Header.test.js
@@ -22,4 +22,37 @@ describe("Header", () => {
     const catHeader = screen.getByRole("heading", { name: "Hello" });
     expect(catHeader).toBeInTheDocument();
   });
+
+  it("should render the my cats text passed in the title prop", () => {
+    render(<Header title="My Cats" />);
+    const catHeader = screen.getByTitle("header-1");
+    expect(catHeader).toBeInTheDocument();
+  });
+
+  it("should render the my cats text passed in the title prop", () => {
+    render(<Header title="Rudra" />);
+    const headerElement = screen.getByTestId("header-2");
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  // Find BY
+  it("should render the my cats text passed in the title prop using Find By", async () => {
+    render(<Header />);
+    const headerElement = await screen.findByText("Hello");
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  // Query BY
+  it("test using query By", () => {
+    render(<Header title="Avinash" />);
+    const headerElement = screen.queryByText("Dog");
+    expect(headerElement).not.toBeInTheDocument();
+  });
+
+  // Get all
+  it("should render Header, Testing with GetByAll", () => {
+    render(<Header title="Sam" />);
+    const headerElements = screen.getAllByText(/Sam/i);
+    expect(headerElements.length).toBe(1);
+  });
 });
