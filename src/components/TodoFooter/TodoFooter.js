f50f04295929594aad907ccc2ad5d47b94f5d89c
avinash.k.v <avinash.k@techsophy.com>
2021-08-07 14:06:01 +0530

    Assertion Test cases are added for TodoFooter

--- src/components/TodoFooter/TodoFooter.js
+++ src/components/TodoFooter/TodoFooter.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import "./TodoFooter.css"
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import "./TodoFooter.css";
+import { Link } from "react-router-dom";
 
-function TodoFooter({
-    numberOfIncompleteTasks
-}) {
-    return (
-        <div className="todo-footer">
-            <p>{numberOfIncompleteTasks} {numberOfIncompleteTasks === 1 ? "task" : "tasks"} left</p>
-            <Link to="/followers">Followers</Link>
-        </div>
-    )
+function TodoFooter({ numberOfIncompleteTasks }) {
+  const [inputStyle, setInputStyle] = useState({
+    opacity: 0,
+  });
+
+  useEffect(() => {
+    if (numberOfIncompleteTasks > 0) {
+      setInputStyle({ opacity: 1 });
+    } else {
+      setInputStyle({ opacity: 0 });
+    }
+  }, [numberOfIncompleteTasks]);
+
+  return (
+    <div className="todo-footer">
+      <p data-testid="para" style={inputStyle}>
+        {numberOfIncompleteTasks}{" "}
+        {numberOfIncompleteTasks === 1 ? "task" : "tasks"} left
+      </p>
+      <Link to="/followers">Followers</Link>
+    </div>
+  );
 }
 
-export default TodoFooter
+export default TodoFooter;
---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,53 @@
+// Assertions
+
+import { render, screen } from "@testing-library/react";
+import TodoFooter from "./../TodoFooter";
+import { BrowserRouter } from "react-router-dom";
+
+const MockTodoFooter = ({ numberOfIncompleteTasks }) => (
+  <BrowserRouter>
+    <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+  </BrowserRouter>
+);
+
+it("should render the correct amount of incomplete tasks", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+  const footerElement = screen.getByText(/5 tasks left/i);
+  expect(footerElement).toBeInTheDocument();
+});
+
+it('should render singular word "task" when numberOfIncompleteTasks is 1', () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const footerElement = screen.getByText(/1 task left/i);
+  expect(footerElement).toBeInTheDocument();
+});
+
+it("should render the footer when the prop is passed", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const footerElement = screen.getByText(/1 task left/i);
+  expect(footerElement).toBeTruthy();
+});
+
+it("footer should not be visible when no prop is passed", () => {
+  render(<MockTodoFooter />);
+  const paragraphElement = screen.getByText("tasks left");
+  expect(paragraphElement).not.toBeVisible();
+});
+
+it("footer should contain paragraph", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText("1 task left");
+  expect(paragraphElement).toContainHTML("p");
+});
+
+it("footer should contain text content", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("para");
+  expect(paragraphElement).toHaveTextContent("1 task left");
+});
+
+it("footer should contain text content", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("para");
+  expect(paragraphElement.textContent).toBe("1 task left");
+});
