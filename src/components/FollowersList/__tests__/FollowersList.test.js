a5bfdfcc776e508d06683f27b4ea469db056c755
avinash.k.v <avinash.k@techsophy.com>
2021-08-10 00:23:52 +0530

    Testing with real API's

---
+++ src/components/FollowersList/__tests__/FollowersList.test.js
@@ -0,0 +1,31 @@
+import { render, screen, findByTestId } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import FollowersList from "./../FollowersList";
+
+// W.R.T. Async and FindByText
+
+const MockFollowersList = () => (
+  <BrowserRouter>
+    <FollowersList />
+  </BrowserRouter>
+);
+
+describe("renders FollowersList", () => {
+  it("should render the component correctly", () => {
+    render(<MockFollowersList />);
+    expect(screen).toMatchSnapshot();
+  });
+
+  it("should render the follower item correctly", async () => {
+    render(<MockFollowersList />);
+    const followerItem = await screen.findByTestId("follower-item-0");
+    expect(followerItem).toBeInTheDocument();
+  });
+
+  // it("should render all the follower items correctly", async () => {
+  //   render(<MockFollowersList />);
+  //   const followerItems = await screen.findAllByTestId(/follower-item/i);
+  //   console.log("I ma followes", followerItems);
+  //   expect(followerItems.length).toBe(5);
+  // });
+});
